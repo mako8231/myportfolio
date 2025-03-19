c06c7a925939a01a322193dae31841b8ec84dd9e
mako8231 <mm.neto2007@gmail.com>
2025-03-19 16:15:53 -0400

    separating into single file components

---
+++ src/components/PresentationSection.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="home" class="jumbotron jumbotron-fluid">
+    <div class="jumbotron-content container-fluid">
+      <img src="../assets/img/photo.jpg" class="profile-landing mx-auto d-block" alt="">
+      <h1 class="presenting display-4">
+        Olá, me chamo Masao Muraoka
+      </h1>
+      <p class="brief-presentation lead">Sou estudante de Engenharia da Computação na UFGD, tenho um background sólido em desenvolvimento web, aplicações Backend, game development. Uso Linux como sistema operacional principal e sou familiar com o uso de linguagens de programação como JavaScript, Python, Golang, PHP, Bash, C, C++ e Lua</p>
+    </div>
+  </div>
+</template>
