9652d0b9090758be3d4d78f2e5d3393adf4054bf
mako8231 <mm.neto2007@gmail.com>
2025-03-21 13:29:15 -0400

    second deploy

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -10,11 +10,11 @@ import ProjectItem from './ProjectComponent/ProjectItem.vue';
       <div class="my-3 d-flex flex-xl-row flex-lg-row flex-md-column flex-sm-column flex-xs-column gap-3">
           <ProjectItem titulo="Kai Space" descricao="Uma engine para a criação de forums focado para artistas (ainda em desenvolvimento)" tecnologias="Javascript, PHP, Vue3, Laravel, Docker" url="https://github.com/mako8231/kai-space"></ProjectItem>
           <ProjectItem titulo="Interpretador Brainf@*k" descricao="Clássico interpretador da linguagem de programação BF" tecnologias="Golang" url="https://github.com/mako8231/brainfuck"></ProjectItem>
-          <ProjectItem titulo="Clone de Instagram" descricao="Projeto realizado enquanto eu estudava a tecnologia Laravel" tecnologias="PHP, Blade, Vue3, Laravel" url="https://github.com/mako8231/brainfuck"></ProjectItem>
+          <ProjectItem titulo="Clone de Instagram" descricao="Projeto realizado enquanto eu estudava a tecnologia Laravel" tecnologias="PHP, Blade, Vue3, Laravel" url="https://github.com/mako8231/ig-clone-laravel"></ProjectItem>
       </div>
       <div class="my-3 d-flex flex-xl-row flex-lg-row flex-md-column flex-sm-column flex-xs-column gap-3">
-          <ProjectItem titulo="Projeto Humanoide" descricao="Um boneco humanoide criado em C/OpenGL para a disciplina de Computação Gráfica" tecnologias="C/C++, OpenGL" url="https://github.com/mako8231/kai-space"></ProjectItem>
-      </div>
+          <ProjectItem titulo="Projeto Humanoide" descricao="Um boneco humanoide criado em C/OpenGL para a disciplina de Computação Gráfica" tecnologias="C/C++, OpenGL" url="https://github.com/mako8231/ProjetoHumanoide"></ProjectItem>
+        </div>
       </div>
   </section>
 </template>
