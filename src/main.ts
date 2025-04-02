5cf9dcdfd072cd2d84dea3b7964390018af80da7
mako8231 <mm.neto2007@gmail.com>
2025-04-02 09:38:40 -0400

    added meta

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import 'bootstrap'
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { useHead } from "@vueuse/head"
 import App from './App.vue'
 
 createApp(App).mount('#app')
+useHead({
+    title: 'Meu Portfolio Pessoal | Masao Muraoka Neto',
+    meta: [
+        { name: 'description', content: 'Portfolio Profissional de Masao Muraoka Neto' },
+        { property: 'og:title', content: 'Meu Portfólio Profissional' },
+        { property: 'og:description', content: 'Confira meus trabalhos e projetos' },
+        { property: 'og:image', content: './assets/img/photo.jpg' }
+    ]
+})
